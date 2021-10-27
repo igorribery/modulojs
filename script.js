4b2f2d7d2a352095df3a52fa56db0c012db2bb8d
Igor <(igorrds1998@gmail.com)>
2021-10-26 23:59:51 -0300

    loop while

--- script.js
+++ script.js
@@ -1,10 +1,11 @@
-function testVerificador() {
-    var x = prompt("Diga um numero:");
+var x = 0;
 
-    if (x > 10) {
-        alert("E maior que 10!!!");
-    } else {
-        alert("Não é maior que 10!");
-    }
+document.write("Iniciando o Loop...<br/>");
+while(x < 10) {
 
+    document.write("Numero: "+x+"<br/>");
+    
+    x++;
 }
+
+document.write("Finalizando o Loop...<br/>");
