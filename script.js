d217706fcaa3d99be959201ca24d792a5c7cab8b
Igor <(igorrds1998@gmail.com)>
2021-10-31 22:59:13 -0300

    eventos de teclado

--- script.js
+++ script.js
@@ -1,15 +1,3 @@
-function apertouMouse() {
-    console.log("Apertou o mouse!");
-}
-function soltouMouse() {
-    console.log("Soltou o mouse!");
-}
-function MouseEmCima() {
-    console.log("Mouse passou em cima!");
-}
-function MouseSaiuCima(){
-    console.log("Mouse saiu de cima!");
-}
-function moveuMouse() {
-    console.log("Mouse foi movido!");
+function apertouTecla(event) {
+    console.log("Apertou alguma tecla: "+event.shiftkey);
 }
