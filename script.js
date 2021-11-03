01ec3513a903f35278706313843148c4fbf1fb7c
Igor <(igorrds1998@gmail.com)>
2021-11-02 22:20:05 -0300

    eventos de formulario

--- script.js
+++ script.js
@@ -1,3 +1,11 @@
-function apertouTecla(event) {
-    console.log("Apertou alguma tecla: "+event.shiftkey);
+function mudouOpcao(objeto) {
+    console.log("Selecionou: "+objeto.value);
+}
+
+function focou() {
+    console.log("Focou no campo 1");
+}
+
+function desfocou() {
+    console.log("desfocou do campo 1");
 }
