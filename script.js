99f63cca37805df3908bfaf96626831a9766f49a
Igor <(igorrds1998@gmail.com)>
2021-11-04 01:04:25 -0300

    jogo estourar bolas

--- script.js
+++ script.js
@@ -1,11 +1,17 @@
-function mudouOpcao(objeto) {
-    console.log("Selecionou: "+objeto.value);
-}
+function addBola() {
+    var bola = document.createElement("div");
+    bola.setAttribute("class", "bola");
 
-function focou() {
-    console.log("Focou no campo 1");
-}
+    var p1 = Math.floor(Math.random() * 500);
+    var p2 = Math.floor(Math.random() * 400);
+    bola.setAttribute("style", "left:"+p1+"px;top:"+p2+"px;");
+    bola.setAttribute("onclick", "estourar(this)");
 
-function desfocou() {
-    console.log("desfocou do campo 1");
+    document.body.appendChild(bola);
+}
+function estourar(elemento) {
+    document.body.removeChild(elemento);
+}
+function iniciar() {
+    setInterval(addBola, 2000);
 }
