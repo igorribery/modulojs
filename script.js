0d3dd05d45372cfea369d0e6012ee0f911f3405b
Igor <(igorrds1998@gmail.com)>
2021-11-24 00:01:55 -0300

    Funcoes auxiliares

--- script.js
+++ script.js
@@ -8,14 +8,22 @@ function Animal() {
         console.log("xiiii.....");
     }
     this.comer = function(kg) {
+        for(var i=0;i<kg;i++) {
+            this.mastigar(i);
+        }
+        
         console.log("hum...");
         this.peso = this.peso + (kg/2);
+    }
+    this.mastigar = function(i){
+        console.log(i+" - nhoc...");
     }
 }
 
 var lulu = new Animal();
 lulu.raca = "Gato";
-lulu.nome ="Lulu;"
+lulu.nome ="Lulu;";
+lulu.comer(10);
 
 var xuxu = new Animal();
 xuxu.raca ="Cao";
