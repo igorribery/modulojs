deea961979cd3b9f42f49da002d544e8766797b8
Igor <(igorrds1998@gmail.com)>
2021-11-23 23:57:03 -0300

    Funções de classe

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
 function Animal() {
     this.raca = "Cao";
     this.nome = "Lulu";
-    this.idade = "5";
-    this.peso = "10";
+    this.idade = 5;
+    this.peso = 10;
 
+    this.fazerXixi = function() {
+        console.log("xiiii.....");
+    }
+    this.comer = function(kg) {
+        console.log("hum...");
+        this.peso = this.peso + (kg/2);
+    }
 }
 
 var lulu = new Animal();
