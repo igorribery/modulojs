2a6afde08922b0bb11d20d8416a2704d430b33cf
Igor <(igorrds1998@gmail.com)>
2021-12-05 20:13:23 -0300

    Web Storage

--- script.js
+++ script.js
@@ -1,30 +1,7 @@
-function Animal() {
-    this.raca = "Cao";
-    this.nome = "Lulu";
-    this.idade = 5;
-    this.peso = 10;
-
-    this.fazerXixi = function() {
-        console.log("xiiii.....");
-    }
-    this.comer = function(kg) {
-        for(var i=0;i<kg;i++) {
-            this.mastigar(i);
-        }
-        
-        console.log("hum...");
-        this.peso = this.peso + (kg/2);
-    }
-    this.mastigar = function(i){
-        console.log(i+" - nhoc...");
-    }
+if (typeof localStorage.nome == "undefined") {
+    localStorage.nome = prompt ("Qual seu nome?");
 }
 
-var lulu = new Animal();
-lulu.raca = "Gato";
-lulu.nome ="Lulu;";
-lulu.comer(10);
+var nome = localStorage.nome;
 
-var xuxu = new Animal();
-xuxu.raca ="Cao";
-xuxu.nome ="Xuxu";
+document.getElementById("info").innerHTML = localStorage.nome;
