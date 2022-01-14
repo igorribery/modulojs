8c2229ce0029caf30512bdaf933b064013df480a
Igor <(igorrds1998@gmail.com)>
2022-01-13 21:24:00 -0300

    descontruindo objetos

--- script.js
+++ script.js
@@ -1,9 +1,17 @@
-let timer;
-function rodar() {
-    timer = setTimeout(function(){
-        document.querySelector('.demo').innerHTML = 'Rodou!';
-    }, 2000);
-}
-function parar() {
-    clearTimeout(timer);
-}
+let pessoa = {
+    nome: 'Bonieky',
+    sobrenome: 'Lacerda',
+    idade: 90,
+    social: {
+        facebook: 'b7web',
+        instagram: 'bonieky'
+    },
+    nomeCompleto: function() {
+        return `${this.nome} ${this.sobrenome}`;
+    }
+};
+
+
+let { nome, sobrenome, idade} = pessoa;
+
+console.log(pessoaNome, sobrenome, idade);
