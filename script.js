bbc4319eec8e423ba664d1a2455958d31f017799
Igor <(igorrds1998@gmail.com)>
2022-01-20 01:39:27 -0300

    fetch 2

--- script.js
+++ script.js
@@ -1,14 +1,29 @@
 function loadPosts() {
     document.getElementById('posts').innerHTML = 'Carregando...';
-
-    fetch('https://jsonplaceholder.typicode.com/posts',)
+    
+    fetch('https://jsonplaceholder.typicode.com/posts')
         .then(function(resultado){
             return resultado.json();
         })
         .then(function(json){
-            document.getElementById('posts').innerHTML = json.length+' posts';
+            montarBlog(json);
         })
         .catch(function(){
             console.log('Deu error!');
-        })
+        });
 }
+
+function montarBlog(lista){
+    let html = '';
+
+    for(let i in lista){
+        html += '<h3>'+lista[i].title+'<h3/>';
+        html += lista[i].body+'<br/>';
+        html += '<hr/>';
+    }
+    document.getElementById('posts').innerHTML = html;
+
+}
+
+
+
