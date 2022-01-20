7adaa4e1c3e32befc2da89ab6859d34eeb02576a
Igor <(igorrds1998@gmail.com)>
2022-01-20 02:26:08 -0300

    async e await

--- script.js
+++ script.js
@@ -1,16 +1,10 @@
-function loadPosts() {
+async function loadPosts(){
     document.getElementById('posts').innerHTML = 'Carregando...';
-    
-    fetch('https://jsonplaceholder.typicode.com/posts')
-        .then(function(resultado){
-            return resultado.json();
-        })
-        .then(function(json){
-            montarBlog(json);
-        })
-        .catch(function(){
-            console.log('Deu error!');
-        });
+
+
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let json = await req.json();
+    montarBlog(json);
 }
 
 function montarBlog(lista){
@@ -20,10 +14,6 @@ function montarBlog(lista){
         html += '<h3>'+lista[i].title+'<h3/>';
         html += lista[i].body+'<br/>';
         html += '<hr/>';
-    }
+    }     
     document.getElementById('posts').innerHTML = html;
-
 }
-
-
-
