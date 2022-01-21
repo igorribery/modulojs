4c0cc287e42d2fb2380073a08d28ed540b6a92cf
Igor <(igorrds1998@gmail.com)>
2022-01-21 12:46:29 -0300

    fecth com post

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-async function loadPosts(){
+async function inserirPost() {
     document.getElementById('posts').innerHTML = 'Carregando...';
 
-
-    let req = await fetch('https://jsonplaceholder.typicode.com/posts');
+    let req = await fetch('https://jsonplaceholder.typicode.com/posts', {
+        method: 'POST',
+        body: JSON.stringify({
+            title: 'Titulo de teste',
+            body: 'Corpo de teste',
+            userId: 4
+        }),
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    });
     let json = await req.json();
-    montarBlog(json);
-}
 
-const montarBlog = (lista) => {
-    let html = '';
-
-    for(let i in lista){
-        html += '<h3>'+lista[i].title+'<h3/>';
-        html += lista[i].body+'<br/>';
-        html += '<hr/>';
-    }     
-    document.getElementById('posts').innerHTML = html;
+    console.log(json);
 }
