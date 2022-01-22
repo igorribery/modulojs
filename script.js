4369fbac60c3b859c3cc7f727e522e283f08e985
Igor <(igorrds1998@gmail.com)>
2022-01-22 16:51:34 -0300

    thumbnails com filereader

--- script.js
+++ script.js
@@ -1,8 +1,13 @@
 function mostrar() {
+    let reader = new FileReader();
     let imagem = document.getElementById('imagem').files[0];
 
-    let img = document.createElement('img');
-    img.src = URL.createObjectURL(imagem);
+    reader.onloadend = function() {
+        let img = document.createElement('img');
+        img.src = reader.result;
 
-    document.getElementById('area').appendChild(img);
+        document.getElementById('area').appendChild(img);
+    }
+
+    reader.readAsDataURL(imagem);
 }
