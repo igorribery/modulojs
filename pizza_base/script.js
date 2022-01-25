d8c86741d6f3793350d709c45e06583d3f0a14eb
Igor <(igorrds1998@gmail.com)>
2022-01-25 19:17:58 -0300

    projeto pizzas parte 4

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -1,15 +1,26 @@
 const qs = (el) => document.querySelector(el);
 const qsa = (el) => document.querySelectorAll(el);
 
-pizzaJson.map((item, index) => {
+pizzaJson.map( (item, index) => {
+
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
 
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
-    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = item.price.toFixed(2);
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    pizzaItem.querySelector('a').addEventListener('click', (e) =>{
+        e.preventDefault();
+
+        qs('.pizzaWindowArea').style.opacity = 0;
+        qs('.pizzaWindowArea').style.display = 'flex';
+        setTimeout(() => {
+            qs('.pizzaWindowArea').style.opacity = 1;
+        }, 200);
     
+    });
+
 
     qs('.pizza-area').append(pizzaItem);
-    
+
 });
