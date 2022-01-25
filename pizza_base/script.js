17a1256e4c02fd720a969069453a5d8e5bef57bc
Igor <(igorrds1998@gmail.com)>
2022-01-25 20:42:20 -0300

    projeto pizzas parte 5

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -5,22 +5,27 @@ pizzaJson.map( (item, index) => {
 
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
 
+    pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
     pizzaItem.querySelector('.pizza-item--price').innerHTML = item.price.toFixed(2);
     pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
     pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
-    pizzaItem.querySelector('a').addEventListener('click', (e) =>{
+    pizzaItem.querySelector('a').addEventListener('click', (e) => {
         e.preventDefault();
+        
+        let key = e.target.closest('.pizza-item').getAttribute('data-key');
+
+        qs('.pizzaBig img').src = pizzaJson[key].img;
+        qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
+        qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+
 
         qs('.pizzaWindowArea').style.opacity = 0;
         qs('.pizzaWindowArea').style.display = 'flex';
         setTimeout(() => {
             qs('.pizzaWindowArea').style.opacity = 1;
         }, 200);
-    
     });
-
-
     qs('.pizza-area').append(pizzaItem);
-
 });
+
