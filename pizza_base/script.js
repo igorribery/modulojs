47aa55d5d957fda06edefa1d93706266131453e9
Igor <(igorrds1998@gmail.com)>
2022-01-25 17:23:21 -0300

    pizza parte 2

---
+++ pizza_base/script.js
@@ -0,0 +1,15 @@
+const qs = (el) => document.querySelector(el);
+const qsa = (el) => document.querySelectorAll(el);
+
+pizzaJson.map((item, index) => {
+    let pizzaItem = qs('.models .pizza-item').cloneNode(true);
+
+    pizzaItem.querySelector('.pizza-item--img img').src = item.img;
+    pizzaItem.querySelector('.pizza-item--price').innerHTML = `R$ ${item.price.toFixed(2)}`;
+    pizzaItem.querySelector('.pizza-item--name').innerHTML = item.name;
+    pizzaItem.querySelector('.pizza-item--desc').innerHTML = item.description;
+    
+
+    qs('.pizza-area').append(pizzaItem);
+    
+});
