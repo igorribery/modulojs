43b41d4ffc0cfb1c6adcaa9f08d46ef91c4be45c
Igor <(igorrds1998@gmail.com)>
2022-01-26 01:39:22 -0300

    project pizza parte 7

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -1,9 +1,13 @@
 const qs = (el) => document.querySelector(el);
 const qsa = (el) => document.querySelectorAll(el);
 
+let modalQt = 1;
+
 pizzaJson.map( (item, index) => {
 
     let pizzaItem = qs('.models .pizza-item').cloneNode(true);
+
+// listagem das pizzas
 
     pizzaItem.setAttribute('data-key', index);
     pizzaItem.querySelector('.pizza-item--img img').src = item.img;
@@ -14,10 +18,22 @@ pizzaJson.map( (item, index) => {
         e.preventDefault();
         
         let key = e.target.closest('.pizza-item').getAttribute('data-key');
+        modalQt = 1;
 
         qs('.pizzaBig img').src = pizzaJson[key].img;
         qs('.pizzaInfo h1').innerHTML = pizzaJson[key].name;
         qs('.pizzaInfo--desc').innerHTML = pizzaJson[key].description;
+        qs('.pizzaInfo--actualPrice').innerHTML = `R$ ${pizzaJson[key].price.toFixed(2)}`;
+
+        qs('.pizzaInfo--size.selected').classList.remove('selected');
+       qsa('.pizzaInfo--size').forEach((size, sizeIndex) => {
+           if(sizeIndex == 2) {
+               size.classList.add('selected');
+           }
+            size.querySelector('span').innerHTML = pizzaJson[key].sizes[sizeIndex];
+       })
+        
+        qs('.pizzaInfo--qt').innerHTML = modalQt;
 
 
         qs('.pizzaWindowArea').style.opacity = 0;
@@ -28,4 +44,17 @@ pizzaJson.map( (item, index) => {
     });
     qs('.pizza-area').append(pizzaItem);
 });
+
+// eventos do MODAL
+
+const closeModal = () => {
+    qs('.pizzaWindowArea').style.opacity = 0;
+    setTimeout(() => {
+        qs('.pizzaWindowArea').style.display = 'none';
+    }, 500);
+}
+
+qsa('.pizzaInfo--cancelButton, pizzaInfo--cancelMobileButton').forEach((item) => {
+    item.addEventListener('click', closeModal);
+})
 
