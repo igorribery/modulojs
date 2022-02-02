95e396f1891bfb82bb3585ae2fa9993f5f7c3991
Igor <(igorrds1998@gmail.com)>
2022-02-02 11:25:57 -0300

    projeto pizza parte 13

--- pizza_base/script.js
+++ pizza_base/script.js
@@ -118,8 +118,15 @@ const updateCart = () => {
     if(cart.length > 0) {
         qs('aside').classList.add('show');
         qs('.cart').innerHTML = '';
+
+        let subtotal = 0;
+        let desconto = 0;
+        let total = 0;
+
         for(let i in cart) {
             let pizzaItem = pizzaJson.find((item) => item.id == cart[i].id);
+            subtotal += pizzaItem.price * cart[i].qt;
+
             let cartItem = qs('.models .cart--item').cloneNode(true);
 
             let pizzaSizeName;
@@ -140,11 +147,32 @@ const updateCart = () => {
             cartItem.querySelector('img').src = pizzaItem.img;
             cartItem.querySelector('.cart--item-nome').innerHTML = pizzaName;
             cartItem.querySelector('.cart--item--qt').innerHTML = cart[i].qt;
+            cartItem.querySelector('.cart--item-qtmenos').addEventListener('click', () => {
+                if(cart[i].qt > 1) {
+                    cart[i].qt--;
+                } else {
+                    cart.splice(i, 1);
+                }
+
+                updateCart();
+            });
+
+            cartItem.querySelector('.cart--item-qtmais').addEventListener('click', () => {
+                cart[i].qt++;
+                updateCart();
+
+            });
 
 
-            qs('.cart').append(cartItem);
-            
+            qs('.cart').append(cartItem);   
         }
+
+        desconto = subtotal * 0.1;
+        total = subtotal - desconto;
+
+        qs('.subtotal span:last-child').innerHTML = `R$ ${subtotal.toFixed(2)}`;
+        qs('.desconto span:last-child').innerHTML = `R$ ${desconto.toFixed(2)}`;
+        qs('.total span:last-child').innerHTML = `R$ ${total.toFixed(2)}`;
 
     } else {
         qs('aside').classList.remove('show');
