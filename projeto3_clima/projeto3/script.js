b294f668c25880988fdf8600c59e2ed750de8757
Igor <(igorrds1998@gmail.com)>
2022-02-28 02:34:33 -0300

    projeto jogodavelha

--- projeto3_clima/projeto3/script.js
+++ projeto3_clima/projeto3/script.js
@@ -1,41 +1,40 @@
 const qs = (el) => document.querySelector(el);
 
-qs('.busca').addEventListener('submit', async (event) => {
-    event.preventDefault();
+qs('.busca').addEventListener('submit', async (e) => {
+    e.preventDefault();
 
     let input = qs('#searchInput').value;
 
-    showWarning('Carregando...');
-
     if(input !== '') {
+        clearInfo();
+        showWarning('Carregando...');
 
         let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(input)}&appid=3beaac56805ad183962d2dd96d2618bd&units=metric&lang=pt_br`;
 
         let results = await fetch(url);
         let json = await results.json();
-        console.log(json);
 
         if(json.cod === 200) {
-            
             showInfo({
                 name: json.name,
                 country: json.sys.country,
                 temp: json.main.temp,
                 tempIcon: json.weather[0].icon,
                 windSpeed: json.wind.speed,
-                windDeg: json.wind.deg
+                windAngle: json.wind.deg
             });
-
         } else {
-            clearInfo();
-            showWarning('Não encontramos essa localização.');
+            showWarning('Não encontramos esta localização.');
         }
 
     } else {
         clearInfo();
     }
-
 });
+
+const showWarning = (msg) => {
+    qs('.aviso').innerHTML = msg;
+};
 
 const showInfo = (json) => {
     showWarning('');
@@ -45,15 +44,10 @@ const showInfo = (json) => {
     qs('.tempInfo').innerHTML = `${json.temp}<sup>ºC</sup>`;
     qs('.temp img').setAttribute('src', `http://openweathermap.org/img/wn/${json.tempIcon}@2x.png`);
     qs('.ventoInfo').innerHTML = `${json.windSpeed}<span>km/h</span>`;
-    qs('.ventoPonto').style.transform = `rotate(${json.windDeg-90}deg)`;
-
-};
-
-const showWarning = (msg) => {
-    qs('.aviso').innerHTML = msg;
-};
+    qs('.ventoPonto').style.transform = `rotate(${json.windAngle-90}deg)`;
+}
 
 const clearInfo = () => {
     showWarning('');
     qs('.resultado').style.display = 'none';
-};
+}
