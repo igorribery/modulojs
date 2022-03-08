661113c921396a0018bb20f6baebadd5481b13ce
Igor <(igorrds1998@gmail.com)>
2022-03-08 13:25:16 -0300

    projectQuiz by myself

--- projeto6_quiz/script.js
+++ projeto6_quiz/script.js
@@ -2,85 +2,77 @@ const qs = (el) => document.querySelector(el);
 const qsa = (el) => document.querySelectorAll(el);
 
 // Initial Data
+
 let currentQuestion = 0;
 let correctAnswers = 0;
 
 showQuestion();
-
-
 // Events
-qs('.scoreArea button').addEventListener('click', resetEvent);
 
+qs('.scoreArea button').addEventListener('click', resetButton);
 
-// Functions
-
+// functions
 
 function showQuestion() {
     if(questions[currentQuestion]) {
         let q = questions[currentQuestion];
-        
-        // barrinha verde de progresso
-        let pct = Math.floor((currentQuestion / questions.length) * 100);
-        qs('.progress--bar').style.width = `${pct}%`;
-        
+
+        let greenBar = Math.floor((currentQuestion / questions.length) * 100);
+        qs('.progress--bar').style.width = `${greenBar}%`;
+
+        qs('.question').innerHTML = q.question;
         qs('.scoreArea').style.display = 'none';
         qs('.questionArea').style.display = 'block';
-        qs('.question').innerHTML = q.question;
 
-        let optionsHtml = '';
+        let optionHtml = ''
         for(let i in q.options) {
-            optionsHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
+            optionHtml += `<div data-op="${i}" class="option"><span>${parseInt(i)+1}</span> ${q.options[i]}</div>`;
         }
-
-        qs('.options').innerHTML = optionsHtml;
-
-        qsa('.options .option').forEach(item => {
-            item.addEventListener('click', optionClickEvent);
+        qs('.options').innerHTML = optionHtml;
+        qsa('.option').forEach(item => {
+            item.addEventListener('click', clickEventOption);
         });
     } else {
         finishQuiz();
     }
 }
-
-function optionClickEvent(e) {
+function clickEventOption(e) {
     let clickedOption = parseInt(e.target.getAttribute('data-op'));
 
     if(questions[currentQuestion].answer === clickedOption) {
         correctAnswers++;
     }
-
     currentQuestion++;
     showQuestion();
-};
+}
 
 function finishQuiz() {
     let points = Math.floor((correctAnswers / questions.length) * 100);
 
-    if(points < 30) {
-        qs('.scoreText1').innerHTML = 'Tá ruim em!?';
-        qs('.scorePct').style.color = '#FF0000';
-    
-    } else if(points >=30 && points < 70) {
-        
+    if(points <30) {
+        qs('.scoreText1').innerHTML = 'Tá ruim hein?';
+        qs('.scorePct').style.color = 'red';
+
+    } else if(points >= 30 && points <70) {
         qs('.scoreText1').innerHTML = 'Muito bom!';
-        qs('.scorePct').style.color = '#FFFF00';
-    
-    } else if(points >= 70) {
-        
+        qs('.scorePct').style.color = 'yellow';
+
+    } else if(points >= 70){
         qs('.scoreText1').innerHTML = 'Parabéns!';
-        qs('.scorePct').style.color = '#0D630D';
+        qs('.scorePct').style.color = 'green';
     }
 
-
+    qs('.scoreArea').style.display = 'block';
+    qs('.questionArea').style.display = 'none';
+    qs('.progress--bar').style.width = `100%`;
     qs('.scorePct').innerHTML = `Acertou ${points}%`;
     qs('.scoreText2').innerHTML = `Você respondeu ${questions.length} e acertou ${correctAnswers}.`;
-    qs('.scoreArea').style.display = 'block';
-    qs('.questionArea').style.display = 'none';    
-    qs('.progress--bar').style.width = '100%';
-};
+}
 
-function resetEvent() {
+function resetButton() {
     correctAnswers = 0;
     currentQuestion = 0;
     showQuestion();
 }
+
+
